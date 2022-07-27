10fcba702f0d8cbb26f13bd5f268d3eff0ab5402
Josh Holmes <josh@glowingmanagement.com>
2022-07-27 14:45:17 +0100

    public navigation

--- src/controllers/views/index.js
+++ src/controllers/views/index.js
@@ -2,6 +2,16 @@ const renderHomePage = (req, res) => {
   return res.render("home", { currentPage: "home" });
 };
 
+const renderLoginPage = (req, res) => {
+  return res.render("login", { currentPage: "login" });
+};
+
+const renderSignupPage = (req, res) => {
+  return res.render("signup", { currentPage: "signup" });
+};
+
 module.exports = {
   renderHomePage,
+  renderLoginPage,
+  renderSignupPage,
 };
--- src/routes/views/index.js
+++ src/routes/views/index.js
@@ -1,11 +1,17 @@
 const { Router } = require("express");
 
-const { renderHomePage } = require("../../controllers/views");
+const {
+  renderHomePage,
+  renderLoginPage,
+  renderSignupPage,
+} = require("../../controllers/views");
 
 const auth = require("../../middlewares/auth");
 
 const router = Router();
 
 router.get("/", renderHomePage);
+router.get("/login", renderLoginPage);
+router.get("/signup", renderSignupPage);
 
 module.exports = router;
