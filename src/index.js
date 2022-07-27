871027828fe3fd1af78f4233ae9a931018d5439c
Josh Holmes <josh@glowingmanagement.com>
2022-07-27 06:56:29 +0100

    create database

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const session = require("express-session");
+const connectSessionSequelize = require("connect-session-sequelize");
 
+const connection = require("./config/connection");
 const routes = require("./routes");
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
+
+const SequelizeStore = connectSessionSequelize(session.Store);
+
+const sessionOptions = {
+  secret: process.env.DB_SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookies: {
+    maxAge: 3600 * 1000,
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+  },
+  store: new SequelizeStore({
+    db: connection,
+  }),
+};
 
 const app = express();
 
+app.use(session(sessionOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(routes);
 
-app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
-);
+const init = async () => {
+  try {
+    // connect to DB
+    await connection.sync({ force: false });
+
+    // server listen on PORT
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to start server | ${error.message}`);
+  }
+};
+
+init();
