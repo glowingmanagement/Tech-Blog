96c1b8237d907345b1344b7083e6f213f9d787a6
Josh Holmes <josh@glowingmanagement.com>
2022-07-27 16:22:26 +0100

    login/signup functions created and link to btns

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+const signupForm = $("#signup-form");
+const loginForm = $("#login-form");
+
+const handleSignup = (event) => {
+  event.preventDefault();
+  console.log("Signup Pressed");
+};
+
+const handleLogin = (event) => {
+  event.preventDefault();
+  console.log("Login Pressed");
+};
+
+signupForm.submit(handleSignup);
+loginForm.submit(handleLogin);
