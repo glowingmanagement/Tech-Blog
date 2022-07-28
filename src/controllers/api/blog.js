7148ba437845a81031e004946a7bbc881c64e5b4
Josh Holmes <josh@glowingmanagement.com>
2022-07-28 08:27:00 +0100

    create post backend

--- src/controllers/api/blog.js
+++ src/controllers/api/blog.js
@@ -1,6 +1,21 @@
 const { User, Blog } = require("../../models");
 
-const createPost = () => {};
+const createPost = async (req, res) => {
+  try {
+    const { id } = req.session.user;
+    const { title, content } = req.body;
+    const createPost = await Blog.create({
+      userId: id,
+      title,
+      description: content,
+    });
+    return res.json({ success: true });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to create post| ${error}`);
+
+    return res.status(500).json({ success: false });
+  }
+};
 
 const updatePost = () => {};
 
